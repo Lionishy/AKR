1b261991c091c1070fb7e09086d0e67148cf4b11
Mark Shevelev <lionishy@gmail.com>
2017-10-07 19:57:52 +0300

    substepping refactored

--- include/IKI/SubsteppingDecorator.class.h
+++ include/IKI/SubsteppingDecorator.class.h
@@ -11,18 +11,12 @@ namespace IKI {
     public:
         int step(VectorSp<T> R, VectorSp<T> K, FVector<T> w, VectorSp<T> &R_out, VectorSp<T> &K_out, FVector<T> &w_out, T &dt) const {
             int res;
-            if (dt_iterator != dt_begin)
-                --dt_iterator;
-            dt = *dt_iterator;
-            do {
-                res = inner_step->step(R,K,w,R_out,K_out,w_out,dt);
-                if (res) {
-                    if (dt_end == ++dt_iterator)
-                        return res;
-                    dt = *dt_iterator;
-                }
-            } while(res);
-                    
+            for (auto dt_iterator = dt_begin; dt_iterator != dt_end; ++dt_iterator) {
+                dt = *dt_iterator;
+                if (!(res = inner_step->step(R,K,w,R_out,K_out,w_out,dt)))
+                    break;
+            }
+
             return res;
         }
 
@@ -33,7 +27,6 @@ namespace IKI {
         std::shared_ptr<go::AbstractStep<T> const> inner_step;
         Col collection;
         typename Col::iterator const dt_begin, dt_end;
-        mutable typename Col::iterator dt_iterator;    
     };
 }//IKI
 
