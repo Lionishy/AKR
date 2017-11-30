e6b258f7706678359a75cc84c843ea63a177a72b
Lionishy <Lionishy@gmail.com>
2017-11-30 14:59:42 +0300

    OmegaCorrector modification

--- include/IKI/OmegaCorrector.class.h
+++ include/IKI/OmegaCorrector.class.h
@@ -36,6 +36,31 @@ namespace IKI {
             return res;
         }
 
+        int correct(VectorSp<T> R, VectorH<T> K, FVector<T> w, VectorSp<T> &R_out, VectorH<T> &K_out, FVector<T> &w_out) const {
+            auto OmegaClosure =
+                [&R,&K,this] (FVector<double> x) -> FVector<double> {
+                    return dr->at(R,K,x);
+                };
+
+            FVector<double> outX, outF;
+            int res =
+                SolveBroydn(
+                      OmegaClosure
+                    , w
+                    , tolerance 
+                    , max_iter 
+                    , outX, outF
+                    , dX, dY
+                );
+
+            if (!res) {
+                R_out = R;
+                K_out = K;
+                w_out = outX;
+            }
+            return res;
+        }
+
         OmegaCorrector(
             std::shared_ptr<go::AbstractDispersionRelation<T> const> dr
             , T tolerance, T dX, T dY, unsigned int max_iter
